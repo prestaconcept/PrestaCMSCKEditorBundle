414912e95de7db2ea9bafc2b81a14860330df9a5
Nicolas Bastien <nbastien@prestaconcept.net>
2013-11-14 09:23:40 +0100

    Bundle set-up

---
+++ DependencyInjection/PrestaCMSCKEditorExtension.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the PrestaCMSCKEditorBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSCKEditorBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PrestaCMSCKEditorExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+    }
+}
---
+++ PrestaCMSCKEditorBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the PrestaCMSCKEditorBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\CMSCKEditorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PrestaCMSCKEditorBundle extends Bundle
+{
+}
